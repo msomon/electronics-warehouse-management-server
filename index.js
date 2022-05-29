2f4605d80f1d677656bf8de78b5e7d19c2098211
msomon <hsomon940@gmail.com>
2022-05-29 12:50:24 +0600

    add

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000 ;
 
 const app =express()
 //  middale ware  //
 app.use(cors())
-app.use(express.json());  
+app.use(express.json()); 
+
+//  verifying  //
+function verifyJWT(req,res,next){
+  const authHeader = req.headers.authorization
+  // console.log(authHeader);
+  if(!authHeader){
+    return res.status(401).send({message:'unauthorized access'})
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(403).send({message:'forbidden access'})
+    }
+    // console.log('decoded' ,decoded);
+    req.decoded =decoded
+    next()
+  })
+}
 
 
 
@@ -16,13 +35,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@warehou
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-console.log(uri);
+// console.log(uri);
 
 async function run(){
 try{
   await client.connect()
   const inventoryCollection = client.db('electronicsWarehouse').collection('inventory')
   const newCollection = client.db('electronicsWarehouse').collection('newInvetory')
+
+  //auth  
+app.post('/login',async (req,res)=>{
+const user = req.body
+const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+  expiresIn:'1d'
+})
+res.send({accessToken})
+})
+
+// service api 
 app.get('/inventory',async(req,res)=>{
 const query = {}
 const cursor = inventoryCollection.find(query)
@@ -34,7 +64,7 @@ res.send(inventory)
 app.put('/inventory/:id',async(req,res)=>{
   const id =req.params.id 
   const updateQuantity =req.body; 
-  console.log(updateQuantity.quantity);
+  // console.log(updateQuantity.quantity);
   const filter = {_id:ObjectId(id)}
   const options = {upsert:true}
   const updateDoc = {
@@ -63,11 +93,22 @@ app.delete('/inventory/:id',async(req,res)=>{
 
 
 // my items  section //
-app.get('/myitems',async(req,res)=>{
-const query = {}
-const cursor = newCollection.find(query)
-const newInventory = await cursor.toArray()
-res.send(newInventory)
+app.get('/myitems',verifyJWT,async(req,res)=>{
+  // const authHeader = req.headers.authorization
+  // console.log(authHeader);
+  const decodedEmail = req.decoded.email
+  const email =req.query.email 
+  // console.log(email);
+  if(email === decodedEmail){
+    const query = {email: email}
+    const cursor = newCollection.find(query)
+    const newInventory = await cursor.toArray()
+    res.send(newInventory)
+
+  }
+  else{
+    res.status(403).send({message:'Forbidden access'})
+  }
 
 })
 
