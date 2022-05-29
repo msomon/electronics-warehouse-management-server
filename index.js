35dca79d808f06eb51d3ab29400b028c00a87eea
msomon <hsomon940@gmail.com>
2022-05-29 12:57:56 +0600

    add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app =express()
 app.use(cors())
 app.use(express.json()); 
 
-//  verifying  //
+//  verifying jwt token  //
 function verifyJWT(req,res,next){
   const authHeader = req.headers.authorization
   // console.log(authHeader);
